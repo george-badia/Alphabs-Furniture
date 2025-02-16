4229f4605500b4a262c1e2ab2a55f3da8906fe80
jojo <georgebadia2019@gmail.com>
2025-02-16 12:07:39 +0300

    all data

---
+++ src/data.jsx
@@ -0,0 +1,223 @@
+
+//import images
+import cabinet_1 from './assets/cabinet1.jpg'
+import cabinet_2 from './assets/cabinet2.jpg'
+import cabinet_3 from './assets/cabinet3.jpg'
+import table_1 from './assets/table1.jpg'
+import table_2 from './assets/table2.jpg'
+import table_3 from './assets/table3.jpg'
+import bed_1 from './assets/bed1.jpg'
+import bed_2 from './assets/bed2.jpg'
+import bed_3 from './assets/bed3.jpg'
+import chandalier_1 from './assets/light1.jpg'
+import chandalier_2 from './assets/light2.jpg'
+import chandalier_3 from './assets/light3.jpg'
+import chair_1 from './assets/chair1.jpg'
+import chair_2 from './assets/chair2.jpg'
+import chair_3 from './assets/chair3.jpg'
+
+
+export const product_category_list = [
+    {
+        product_name: "All",
+    },
+    {
+        product_name: "Chair",
+    },
+    {
+        product_name: "Table",
+    },
+    {
+        product_name: "Bed", 
+    },
+    {
+        product_name: "Cabinet",
+    },
+    {
+        product_name: "Lightning",
+    }
+]
+
+
+export const product_list = [
+    {
+        id: 1,
+        name: "High Quality Living Room Chair",
+        image: chair_1,
+        price: 1200,
+        category: "Chair",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+        },
+    },
+    {
+        id: 2,
+        name: "Luxurious Wooden Center Table",
+        image: table_1,
+        price: 1800,
+        category: "Table",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 3,
+        name: "Luxury Minimal Quality Bed",
+        image: bed_1,
+        price: 1200,
+        category: "Bed",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 4,
+        name: "Wooden Custom Made Chandelier",
+        image: chandalier_1,
+        price: 1200,
+        category: "Lightning",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+           shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 5,
+        name: "Black Sleek Living Room Cabinet",
+        image: cabinet_1,
+        price: 1800,
+        category: "Cabinet",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 6,
+        name: "White Wooden Cabinet",
+        image: cabinet_2,
+        price: 1200,
+        category: "Cabinet",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+        },
+    },
+    {
+        id: 7,
+        name: "Luxuriour Round Table",
+        image: table_2,
+        price: 1800,
+        category: "Table",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 8,
+        name: "Luxurious Electric Heated Bed",
+        image: bed_2,
+        price: 1200,
+        category: "Bed",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 9,
+        name: "Gold Wooden Chandalier",
+        image: chandalier_2,
+        price: 1200,
+        category: "Lightning",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+           shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 10,
+        name: "Brown Living Room Sofa",
+        image: chair_2,
+        price: 18,
+        category: "Chair",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 11,
+        name: "Kithcen Wooden Cabinet ",
+        image: cabinet_3,
+        price: 12,
+        category: "Cabinet",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+        },
+    },
+    {
+        id: 12,
+        name: "Luxury Double Side Table",
+        image: table_3,
+        price: 18,
+        category: "Table",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 13,
+        name: "Electric Heated Luxurious Bed",
+        image: bed_3,
+        price: 12,
+        category: "Bed",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 14,
+        name: "Mushroom Shaped Electric Light",
+        image: chandalier_3,
+        price: 12,
+        category: "Lightning",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+           shipping: 'On Orders above $100'
+          },
+    },
+    {
+        id: 15,
+        name: "Imported luxury Chair",
+        image: chair_3,
+        price: 18,
+        category: "Chair",
+        productDetail :{
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illoat. Repudiandae ratione impedit delectus consectetur. Aspernaturvero obcaecati placeat ab distinctio unde ipsam molestias atqueratione delectus blanditiis nemo eius dignissimos doloremque quaealiquid maiores id tempore consequatur, quod pariatur saepe.',
+            delivery: '5-7 Days from order date.',
+            shipping: 'On Orders above $100'
+          },
+    },    
+]
